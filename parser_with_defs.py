28296403d0d1f7e1734685ae85a03a9d6e343db7
Endy <endyrlx@gmail.com>
2022-12-14 00:42:36 +0300

    Add files via upload

---
+++ parser_with_defs.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+print("Запуск...")
+options = ChromeOptions()
+options.headless = True
+options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+options.add_argument("--incognito")
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1024, 768)
+#    url = str(sys.argv[1])
+
+def buysomegoods():
+    url = "https://vkusvill.ru/goods/supermarket/"
+    driver.get(url)
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "CartButton__inner")))
+    driver.find_element(By.CLASS_NAME,"CartButton__inner").click()
+    driver.save_screenshot("buysomegoods.png")
+
+def enteraddress():
+    url = "https://vkusvill.ru/"
+    driver.get(url)
+    driver.find_element(By.XPATH, "/html/body/div[2]/header/div/div[1]/div/div/div[5]/div/div[2]/div[1]/div/div/a/div[2]").click()
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "js-delivery__shopselect--form-address")))
+    element = driver.find_element(By.CLASS_NAME, "js-delivery__shopselect--form-address")
+    time.sleep(1)
+    deliveryaddress = input("Введи адрес доставки: ")
+    element.send_keys(f"{deliveryaddress}")
+    print("")
+    time.sleep(3)
+    element = driver.find_element(By.CLASS_NAME, "VV_Input__Clear")
+    ac = ActionChains(driver)
+    ac.move_to_element(element).send_keys('\ue015').send_keys('\ue007').perform()
+    time.sleep(5)
+    driver.find_element(By.CLASS_NAME,"VV_RWayChoiceModal__ButtonWrp").click()
+    time.sleep(4)
+    driver.find_element(By.CLASS_NAME,"VV_Button._desktop-lg._tablet-lg._mobile-md.js-delivery__shopselect-terms-accept._block").click()
+    time.sleep(3)
+    driver.save_screenshot("enteraddress.png")
+
+def getgreenlabels():
+    url = "https://vkusvill.ru/cart/"
+    driver.get(url)
+    try:
+        element = driver.find_element(By.CLASS_NAME,"ProductCards__item.swiper-slide.swiper-slide-active")
+        grrenlabels = element.get_attribute("aria-label").split("/")
+        maxgreenlabels = int(grrenlabels[-1])
+        driver.find_element(By.CLASS_NAME, "OrderFormProdSliderSwiper.swiper-container.js-order-form-green-labels-slider.swiper-container-initialized.swiper-container-horizontal.swiper-container-pointer-events.swiper-container-free-mode").value_of_css_property("width: 1500px")
+        print(f"= = = = Найдено {maxgreenlabels} зеленых ценников = = = =") #беру количество карточек товаров из рубрики "зеленые ценники"
+        for i in range(maxgreenlabels):
+            print("Карточка товара номер ", i+1)
+            element = driver.find_element(By.XPATH, f"/html/body/div[2]/main/div[1]/div[1]/form/div/div[2]/div[9]/div[3]/div/div/div[{i+1}]/div/div/div[2]/a")
+            print(element.get_attribute("title"))
+            price = driver.find_element(By.XPATH, f"/html/body/div[2]/main/div[1]/div[1]/form/div/div[2]/div[9]/div[3]/div/div/div[{i+1}]/div/div/div[2]/div[1]/span[1]/span[1]")
+            print(price.text if price.text != "" else "Нет цены")
+            print("===========================")
+            time.sleep(1)
+    except:
+        print("Не могу найти зеленые ценники. Возможно, сейчас их нет или что-то другое пошло не так")
+    driver.save_screenshot("getgreenlabels.png")
+    driver.quit()
+
+
+if __name__ == '__main__':
+    enteraddress()
+    buysomegoods()
+    getgreenlabels()
+
